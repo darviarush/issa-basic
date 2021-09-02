a539733277725fe62588420fb5b3fc94d5a13456
Yaroslav O. Kosmina <darviarush@mail.ru>
2021-09-02 18:09:08 +0300

    master save

--- bin/issa-basic.js
+++
@@ -1,4 +0,0 @@
-IssaBasic = require("../lib/issa-basic.js")
-
-// Компилит класс и метод
-new IssaBasic().compile(process.argv[2], process.argv[3])
--- old/src/gen_code_c.h
+++
@@ -1,7 +0,0 @@
-#ifndef __GEN_CODE_C__H__
-#define __GEN_CODE_C__H__
-
-
-void gen_code_c_prog(char* outfile, char* class);
-
-#endif
